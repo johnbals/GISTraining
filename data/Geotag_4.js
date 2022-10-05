8008c44f5accfa529416224ffe9c3dad60a1cc49
johnbals <115063388+johnbals@users.noreply.github.com>
2022-10-05 14:39:03 +0800

    Add files via upload

---
+++ data/Geotag_4.js
@@ -0,0 +1,1 @@
+var json_Geotag_4 = {"type":"FeatureCollection","name":"Geotag_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Name":"Geotag","Folder":"C:\\RLGM\\Projects\\Exercise7MapLay_Out.jpeg"},"geometry":{"type":"Point","coordinates":[124.868200453691614,8.946020934787674]}}]}
